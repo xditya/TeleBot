b9c7f91d97c8b1d4f5c885d35198801c207d00ef
xditya <58950863+xditya@users.noreply.github.com>
2020-03-29 17:47:29 +0530

    Uploaded New Plugin

---
+++ userbot/plugins/ss.py
@@ -0,0 +1,15 @@
+from telethon import events, errors, functions, types
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd("ss"))
+async def _(event):
+    if event.fwd_from:
+        return
+    NO_OF_SCSS = int(event.text.split(" ", maxsplit=1)[1]) 
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+        
+    for I in range(NO_OF_SCSS):
+      await event.client(functions.messages.SendScreenshotNotificationRequest(peer=event.chat_id, reply_to_msg_id=42))
+    await event.delete() 
