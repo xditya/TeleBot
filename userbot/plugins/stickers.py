e8feb03ae5018e1d2bc57bda216182cd252b69aa
xditya <58950863+xditya@users.noreply.github.com>
2020-03-31 09:20:30 +0530

    Update stickers.py

--- userbot/plugins/stickers.py
+++ userbot/plugins/stickers.py
@@ -46,7 +46,7 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"@A_Dark_Princ3 ki gufa me {userid}"
+    packname = f"@TeleBotHelp kang by {userid}"
     packshortname = f"TeleBotHelp_Stickers_Stolen_by_{userid}"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
