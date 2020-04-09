27ca861e0244df3a9fa82b4661badc176bd1239d
xditya <58950863+xditya@users.noreply.github.com>
2020-04-09 19:42:32 +0530

    Uploaded New Plugin

---
+++ userbot/plugins/gay.py
@@ -0,0 +1,35 @@
+#python 3.7.1
+
+import time
+import os,sys
+"""Available Commands:
+.wtf"""
+
+from telethon import events
+
+import asyncio
+
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd("(.*)"))
+
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(0, 5)
+    input_str = event.pattern_match.group(1)
+    if input_str == "gay":
+        await event.edit(input_str)
+        animation_chars = [
+            "HI USER",
+            "HI USER , WAIT",
+            "HI USER , WAIT ARE YOU....",
+            "HI USER , WAIT ARE YOU UMH...",
+            "HI USRR , WAIT ARE YOU A GAY??😬"
+        ]
+
+        for i in animation_ttl:
+        	
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i %5 ])
