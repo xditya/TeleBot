f99c980e9983003bbe1bcb382c2326cc39800285
xditya <58950863+xditya@users.noreply.github.com>
2020-04-20 19:56:03 +0530

    Update carbonRGB.py

--- userbot/plugins/carbonRGB.py
+++ userbot/plugins/carbonRGB.py
@@ -100,7 +100,7 @@ async def carbon_api(e):
    await e.client.send_file(
          e.chat_id,
          file,
-         caption="Carbonised by [IndianBot](https://t.me/IndianArMyGiveaway)",
+         caption="Carbonised by [TeleBot](https://t.me/TeleBotHelp)",
          force_document=False,
          reply_to=e.message.reply_to_msg_id,
          )
