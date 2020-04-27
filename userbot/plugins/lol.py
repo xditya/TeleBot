5bcc7863fe9d6eae0755ada6f5f142276957c04e
xditya <58950863+xditya@users.noreply.github.com>
2020-04-28 05:11:42 +0530

    Uploaded New Plugin

---
+++ userbot/plugins/lol.py
@@ -0,0 +1,17 @@
+# (c) @UniBorg
+
+from telethon import events
+import asyncio
+from collections import deque
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd(pattern=r"lol"))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("😂🤣😂🤣😂🤣"))
+	for _ in range(999):
+		await asyncio.sleep(1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+    
