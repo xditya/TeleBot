f221fea921996907f364d74521c967eea4802dd0
xditya <58950863+xditya@users.noreply.github.com>
2020-05-18 18:01:02 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -6,12 +6,13 @@ from platform import uname
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @TeleBotHelp"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet, check pinned in @TeleBotHelp"
 
 @command(outgoing=True, pattern="^.alive$")
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("**`Hey! I'm alive. All systems online and functioning normally... ψ(｀∇´)ψ`**\n\n"
+    await alive.edit("   **Welcome To TeleBot ** \n\n"
+        "**`Hey! I'm alive. All systems online and functioning normally... ψ(｀∇´)ψ`**\n\n"
                       "` 🔸 Telethon version:` **6.9.0**\n` 🔹 Python:` **3.7.3** \n` 🔸 Fork by:` @xditya\n"
                      "` 🔹 Bot created by:` [Aditya 🇮🇳](tg://user?id=719195224)\n"
                      "` 🔸 Database Status:` **All OK 👌!**\n"
