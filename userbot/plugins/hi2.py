21ffc9488f5d827ff3d10c2f1f9e57e30d24a24c
Indian Bhai <61813271+indianbhaiya@users.noreply.github.com>
2020-05-24 11:26:07 +0530

    hi2 xd

---
+++ userbot/plugins/hi2.py
@@ -0,0 +1,15 @@
+''' Whatever Plugin by Noobs of Telegram i.e. @pureindialover 
+    custom cmd: .hi2
+'''
+
+from telethon import events
+import asyncio
+import os
+import sys
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd(pattern=r"hi2"))
+async def hi(event):
+    if event.fwd_from:
+        return
+    await event.edit("🌺✨✨🌺✨🌺🌺🌺\n🌺✨✨🌺✨✨🌺✨\n🌺🌺🌺🌺✨✨🌺✨\n🌺✨✨🌺✨✨🌺✨\n🌺✨✨🌺✨🌺🌺🌺\n☁☁☁☁☁☁☁☁")
