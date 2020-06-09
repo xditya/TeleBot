02299560afff87bafba1b139d3e4380ad2d6ab79
xditya <58950863+xditya@users.noreply.github.com>
2020-06-09 16:45:39 +0530

    Update wtf.py

--- userbot/plugins/wtf.py
+++ userbot/plugins/wtf.py
@@ -24,7 +24,7 @@ async def _(event):
             "What The",
             "What The F",
             "What The F Brah",
-            "What The F Brah\nhttps://telegra.ph//file/f3b760e4a99340d331f9b.jpg"
+            "[What The F Brah](https://telegra.ph//file/f3b760e4a99340d331f9b.jpg)"
         ]
 
         for i in animation_ttl:
