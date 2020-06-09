6c3281569b9a83fc97cb2d15949b12fa727795b2
xditya <58950863+xditya@users.noreply.github.com>
2020-06-09 16:50:34 +0530

    Update gamerpfp.py

--- userbot/plugins/gamerpfp.py
+++ userbot/plugins/gamerpfp.py
@@ -1,4 +1,4 @@
-#Made By @Nihinivi Keep Credits If You Are Goanna Kang This Lol
+#Made By @TeleBotComms Keep Credits If You Are Goanna Kang This Lol
 
 #And Thanks To The Creator Of Autopic This Script Was Made from Snippets From That Script
 
@@ -62,7 +62,7 @@ async def animepp():
 
 async def main(event):
 
-    await event.edit("**Starting Gamer Profile Pic.\n\nModded by @ViperAdnan") #Owner @NihiNivi
+    await event.edit("**Starting Gamer Profile Pic.\n\nModded by[TeleBot](https://github.com/xditya/TeleBot)") #Owner @NihiNivi
 
     while True:
 
