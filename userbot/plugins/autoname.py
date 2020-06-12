127cc460971e7522a44a900d80e4843c6da6c44e
xditya <58950863+xditya@users.noreply.github.com>
2020-06-12 08:27:24 +0530

    Update autoname.py

--- userbot/plugins/autoname.py
+++ userbot/plugins/autoname.py
@@ -20,7 +20,7 @@ async def _(event):
     while True:
         DM = time.strftime("%d-%m-%y")
         HM = time.strftime("%H:%M")
-        name = f"🕒{HM} ⚡{DEFAULTUSER}⚡ 📅{DM}"
+        name = f"🕒{HM} ⚡{DEFAULTUSER}⚡ {DM} 📆"
         logger.info(name)
         try:
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
