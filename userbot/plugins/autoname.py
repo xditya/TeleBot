5ddf83b44c1d6c50ebe1a3c132f7d4e67205e03c
xditya <58950863+xditya@users.noreply.github.com>
2020-06-12 05:35:29 +0530

    Now shows your name :)

--- userbot/plugins/autoname.py
+++ userbot/plugins/autoname.py
@@ -1,68 +1,40 @@
 """Auto Profile Updation Commands
-
 .autoname"""
-
 from telethon import events
-
 import asyncio
-
 import time
-
 from telethon.tl import functions
-
 from telethon.errors import FloodWaitError
-
 from uniborg.util import admin_cmd
+from userbot import ALIVE_NAME
 
-DEL_TIME_OUT = 70
+
+DEL_TIME_OUT = 60
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TeleBot"
+
 
 @borg.on(admin_cmd(pattern="autoname"))  # pylint:disable=E0602
-
 async def _(event):
-
     if event.fwd_from:
-
         return
-
     while True:
-
         DM = time.strftime("%d-%m-%y")
-
         HM = time.strftime("%H:%M")
-
-        name = f"{HM} Aditya 🇮🇳 {DM}"
-
+        name = f"🕒{HM} ⚡{DEFAULTUSER}⚡ 📅{DM}"
         logger.info(name)
-
         try:
-
             await borg(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
-
                 first_name=name
-
             ))
-
         except FloodWaitError as ex:
-
             logger.warning(str(e))
-
             await asyncio.sleep(ex.seconds)
-
     
-
         # else:
-
             # logger.info(r.stringify())
-
             # await borg.send_message(  # pylint:disable=E0602
-
             #     Config.PRIVATE_GROUP_BOT_API_ID,  # pylint:disable=E0602
-
             #     "Successfully Changed Profile Name"
-
             # )
-
         await asyncio.sleep(DEL_TIME_OUT)
-
-    await event.edit(f"Auto Name has been started Master") 
-
+    await event.edit(f"Auto Name has been started...") 
