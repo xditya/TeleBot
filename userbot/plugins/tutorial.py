8d3b9472edc1fc45848dd81d5b3fe02d7db01a8b
xditya <58950863+xditya@users.noreply.github.com>
2020-06-14 18:04:08 +0530

    Update tutorial.py

--- userbot/plugins/tutorial.py
+++ userbot/plugins/tutorial.py
@@ -9,4 +9,4 @@ async def join(e):
 
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
 
-        await e.edit("Get a userbot like mine!!! Watch [this video tutorial](https://youtu.be/XmvdDHiIDb4) on deploying...")
+        await e.edit("Get a userbot like mine!! Watch [this video tutorial](https://youtu.be/XmvdDHiIDb4) on deploying...")
