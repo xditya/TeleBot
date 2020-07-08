4aaae29a318ace38b8c1661c98a52037780fe2f3
xditya <58950863+xditya@users.noreply.github.com>
2020-07-08 16:07:14 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -37,7 +37,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """Userbot Helper provided by [TeleBot](https://telegra.ph/TeleBot-07-08) \n\n [More help](https://t.me/TeleBotHelpChat) \n
+            help_string = """Userbot Helper provided by [TeleBot](https://telegra.ph/TeleBot-07-08) \n [More help](https://t.me/TeleBotHelpChat) \n
 `Userbot Helper to reveal all the commands of `**[TeleBot](https://github.com/xditya/TeleBot)**"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
