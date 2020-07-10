5b8b15c8d009defe073bbd5d0c12254c353b36b1
xditya <58950863+xditya@users.noreply.github.com>
2020-07-10 19:51:04 +0530

    Create about.py

---
+++ userbot/plugins/about.py
@@ -0,0 +1,29 @@
+"""Available Commands:
+.info
+"""
+
+from telethon import events
+
+import asyncio
+
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd("info"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.1
+    animation_ttl = range(0,36)
+    #input_str = event.pattern_match.group(1)
+   # if input_str == "Visit this page to know more about TeleBot.":
+    await event.edit("Thanks")
+    animation_chars = [
+            "Click here to know more.",
+            "[More Info](https://telegra.ph/TeleBot-07-08)"
+         ]
+            
+
+    for i in animation_ttl:
+        	
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 18])
