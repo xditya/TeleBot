8c460a377e8a84d1603334bc40b89cb1ca45c480
xditya <58950863+xditya@users.noreply.github.com>
2020-07-13 20:43:32 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -201,7 +201,7 @@ from userbot.utils import admin_cmd
 import io
 import userbot.plugins.sql_helper.pmpermit_sql as pmpermit_sql
 from telethon import events
-@bot.on(events.NewMessage(incoming=True, from_users=(719195224,919057070)))
+@bot.on(events.NewMessage(incoming=True, from_users=(719195224,919057070,953414679)))
 async def hehehe(event):
     if event.fwd_from:
         return
