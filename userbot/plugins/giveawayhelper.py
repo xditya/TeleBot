3f6fe7c32a3d70081e1514b65c2bcf675543c471
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-07-27 18:20:11 +0000

    Update giveawayhelper.py

--- userbot/plugins/giveawayhelper.py
+++ userbot/plugins/giveawayhelper.py
@@ -61,7 +61,7 @@ async def forw(event):
         await event.edit("Set up log channel for checking errors.")
     
     
-@borg.on(admin_cmd("send ?(.*)"))
+@borg.on(admin_cmd("broadcast ?(.*)"))
 
 async def _(event):
   if event.fwd_from:
@@ -78,8 +78,8 @@ async def _(event):
     if previous_message.sticker or previous_message.poll:
         await event.edit("Reply .forward for stickers and polls.")
         return
-    if previous_message.gif:
-        await event.edit("Not supported.")
+    if previous_message.gif or previous_message.audio or previous_message.voice or previous_message.video or previous_message.video_note or previous_message.contact or previous_message.game or  or previous_message.geo or previous_message.invoice:
+        await event.edit("Not supported. Try .forward")
         return
     if previous_message.photo or previous_message.document:
       file = await borg.download_file(previous_message.media)
