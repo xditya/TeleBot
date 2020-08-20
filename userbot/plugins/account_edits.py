302073938164f519e2f515e3f9811fb8f40bc744
xditya <58950863+xditya@users.noreply.github.com>
2020-08-20 20:38:28 +0530

    Update and rename account_profile.py to account_edits.py

--- userbot/plugins/account_edits.py
+++ userbot/plugins/account_edits.py
@@ -2,12 +2,21 @@
 # Kangers, don't remove this line 
 # @its_xditya
 
-"""COMMAND : .pbio, .ppic, .pname"""
+"""COMMAND : .pbio, .ppic, .pname, .delpfp {n}"""
 
 import os
 from telethon.tl import functions
 from userbot.utils import admin_cmd
-
+from telethon.errors import ImageProcessFailedError, PhotoCropSizeSmallError
+from telethon.errors.rpcerrorlist import (PhotoExtInvalidError,
+                                          UsernameOccupiedError)
+from telethon.tl.functions.account import (UpdateProfileRequest,
+                                           UpdateUsernameRequest)
+from telethon.tl.functions.channels import GetAdminedPublicChannelsRequest
+from telethon.tl.functions.photos import (DeletePhotosRequest,
+                                          GetUserPhotosRequest,
+                                          UploadProfilePhotoRequest)
+from telethon.tl.types import InputPhoto, MessageMediaPhoto, User, Chat, Channel
 
 @borg.on(admin_cmd("pbio (.*)"))  # pylint:disable=E0602
 async def _(event):
@@ -74,3 +83,29 @@ async def _(event):
         os.remove(photo)
     except Exception as e:  # pylint:disable=C0103,W0703
         logger.warn(str(e))  # pylint:disable=E0602
+
+@borg.on(admin_cmd(pattern="delpfp ?(.*)"))
+async def remove_profilepic(delpfp):
+    """ For .delpfp command, delete your current profile picture in Telegram. """
+    group = delpfp.text[8:]
+    if group == 'all':
+        lim = 0
+    elif group.isdigit():
+        lim = int(group)
+    else:
+        lim = 1
+
+    pfplist = await delpfp.client(
+        GetUserPhotosRequest(user_id=delpfp.from_id,
+                             offset=0,
+                             max_id=0,
+                             limit=lim))
+    input_photos = []
+    for sep in pfplist.photos:
+        input_photos.append(
+            InputPhoto(id=sep.id,
+                       access_hash=sep.access_hash,
+                       file_reference=sep.file_reference))
+    await delpfp.client(DeletePhotosRequest(id=input_photos))
+    await delpfp.edit(
+        f"`Successfully deleted {len(input_photos)} profile picture(s).`")
