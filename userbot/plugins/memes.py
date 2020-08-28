216676b128794e664a7974cd75e2dc6612d8d0eb
xditya <58950863+xditya@users.noreply.github.com>
2020-08-28 20:22:52 +0530

    lol

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -882,7 +882,7 @@ async def vapor(vpr):
 @register(outgoing=True, pattern="^.repo$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Click [here](https://github.com/xditya/TeleBot) to get my source code :) ")
+        await e.edit("Click [here](https://github.com/xditya/TeleBot) to get my source code :)\nDo check @TeleBotSupport ")
 			  
 			  
 @register(outgoing=True, pattern="^.str(?: |$)(.*)")
