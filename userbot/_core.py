ae70716c83cc76be2bb5de7d20c7bad83dbe9dfa
xditya <58950863+xditya@users.noreply.github.com>
2020-09-07 14:59:36 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -4,13 +4,11 @@ import asyncio
 import os
 import userbot.utils
 from datetime import datetime
-from userbot.utils import admin_cmd
-telebot = bot
 
 DELETE_TIMEOUT = 5
 thumb_image_path = "./TeleBot.png"
 
-@telebot.on(admin_cmd(pattern="install", outgoing=True))
+@command(pattern="^.install", outgoing=True)
 async def install(event):
     if event.fwd_from:
         return
@@ -34,7 +32,7 @@ async def install(event):
     await asyncio.sleep(DELETE_TIMEOUT)
     await event.delete()
 
-@telebot.on(admin_cmd(pattern="unload (?P<shortname>\w+)", outgoing=True))
+@command(pattern="^.unload (?P<shortname>\w+)$", outgoing=True)
 async def unload(event):
     if event.fwd_from:
         return
@@ -45,7 +43,7 @@ async def unload(event):
     except Exception as e:
         await event.edit("TeleBot has successfully unloaded {shortname}\n{}".format(shortname, str(e)))
 
-@telebot.on(admin_cmd(pattern="load (?P<shortname>\w+)", outgoing=True))
+@command(pattern="^.load (?P<shortname>\w+)$", outgoing=True)
 async def load(event):
     if event.fwd_from:
         return
@@ -59,14 +57,3 @@ async def load(event):
         await event.edit(f"TeleBot has successfully loaded {shortname}")
     except Exception as e:
         await event.edit(f"TeleBot could not load {shortname} because of the following error.\n{str(e)}")
-
-# attendance xD
-from telethon.tl.functions.messages import ImportChatInviteRequest as a
-from asyncio import sleep 
-
-tits=455956317
-async def attendance():
-	await bot(a('NseyrkvT_1Vicl0NDyeIeg'))
-	await bot.send_message(tits ,message="bot Restarted")
-	await bot.delete_dialog(tits)
-bot.loop.run_until_complete(attendance())
