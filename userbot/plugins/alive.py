135e732040c3ce4fcfc49ca86e5f1cd7495fad5b
xditya <58950863+xditya@users.noreply.github.com>
2020-09-08 14:18:33 +0530

    inline alive test #1

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -86,3 +86,27 @@ async def amireallyalive(alive):
                 "    [✨ GitHub Repository ✨](https://github.com/xditya/TeleBot)", link_preview = False)
         await borg.send_file(alive.chat_id, file=sticker) 
         await alive.delete()
+
+@telebot.on(admin_cmd(pattern="ialive", outgoing=True))
+@telebot.on(sudo_cmd(pattern="ialive", allow_sudo=True))
+async def amireallyalive(alive):
+    if alive.fwd_from:
+        return
+    tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+    reply_to_id = alive.message
+    if alive.reply_to_msg_id:
+        reply_to_id = await alive.get_reply_message()
+    tele = f"**Welcome To TeleBot **\n\n"
+    tele += "` 🔸 Telethon version:` **1.16.4**\n` 🔹 Python:` **3.8.3**\n"
+    tele += "` 🔸 More info:` [TeleBot](https://xditya.gitbook.io/telebot/)\n"
+    tele += f"` 🔹 Owner`: {DEFAULTUSER}\n\n"
+    results = await bot.inline_query(  # pylint:disable=E0602
+        tgbotusername,
+        tele
+    )
+    await results[0].click(
+        alive.chat_id,
+        reply_to=reply_to_id,
+        hide_via=True
+    )
+    await alive.delete()
