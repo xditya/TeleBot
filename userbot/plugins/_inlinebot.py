17bbd72dc7f243b529b1638e39c7c67310f6766e
xditya <58950863+xditya@users.noreply.github.com>
2020-09-08 16:21:26 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -113,7 +113,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 )
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"telestatus")))
     async def on_plug_in_callback_query_handler(event):
-        statustext = tele_alv()
+        statustext = telealive()
         reply_pop_up_alert = statustext
         await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
