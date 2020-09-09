f5aa1f5969e5d0e3744a45b989f7eae91e53f87d
xditya <58950863+xditya@users.noreply.github.com>
2020-09-09 11:47:33 +0530

    Update edits.py

--- userbot/plugins/edits.py
+++ userbot/plugins/edits.py
@@ -578,7 +578,7 @@ async def _(event):
             await event.edit(animation_chars[i % 549755813888])    
             
             
-@borg.on(admin_cmd(pattern=r"star", outgoing=True))
+@borg.on(admin_cmd(pattern=r"bfly", outgoing=True))
 async def _(event):
 	if event.fwd_from:
 		return
