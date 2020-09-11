dc4c9a1fa6d97e433229a3bb3e38d35629dd6498
xditya <58950863+xditya@users.noreply.github.com>
2020-09-11 19:24:34 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -86,6 +86,9 @@ if bool(ENV):
     CHROME_DRIVER = os.environ.get("CHROME_DRIVER", None)
     GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", None)
 
+    # For bit.ly plugin
+    BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
+
     # OpenWeatherMap API Key
     OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
 
