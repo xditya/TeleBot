303ebfe2f5c03cd1a0b45737be8e666b457733bb
xditya <58950863+xditya@users.noreply.github.com>
2020-09-13 16:20:09 +0530

    adding unfban

--- userbot/plugins/superfban.py
+++ userbot/plugins/superfban.py
@@ -1,4 +1,4 @@
-#  ©2020 TeleBot
+#  (c)2020 TeleBot
 #
 # You may not use this plugin without proper authorship and consent from @TeleBotSupport
 #
@@ -69,8 +69,8 @@ async def _(event):
     try:
         await telebot.send_message(chat, f"/start")
     except:
-        await event.edit("FBAN_GROUP_ID is incorrect so using current chat id.")
-        chat = await event.get_chat()
+        await event.edit("FBAN_GROUP_ID is incorrect.")
+        return
     await asyncio.sleep(3)
     if Config.EXCLUDE_FED:
         excludeFed = Config.EXCLUDE_FED.split("|")
@@ -88,7 +88,68 @@ async def _(event):
         await asyncio.sleep(3)
     await event.edit(f"SuperFBan Completed. Affected {len(fedList) - exCount} feds.\n#TB")  
 # By @HeisenbergTheDanger, @its_xditya
+@telebot.on(admin_cmd("superunfban ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("Starting a Mass-UnFedBan...")
+    FBAN = event.pattern_match.group(1)
+    
+    if Config.FBAN_GROUP_ID:
+        chat = Config.FBAN_GROUP_ID
+    else:
+        chat = await event.get_chat()
+    fedList = []
+    async with telebot.conversation("@MissRose_bot") as bot_conv:
+        await bot_conv.send_message("/start")
+        await bot_conv.send_message("/myfeds")
+        response = await bot_conv.get_response()
+        if "make a file" in response.text:
+            await asyncio.sleep(1)
+            await response.click(0)
+            fedfile = await bot_conv.get_response()
+            if fedfile.media:
+                downloaded_file_name = await telebot.download_media(
+                fedfile,
+                "fedlist"
+                )
+                file = open(downloaded_file_name, 'r')
+                lines = file.readlines()
+                for line in lines:
+                    fedList.append(line[:line.index(":")])
+            else:
+                return
+        if "You can only use fed commands once every 5 minutes" in response.text:
+            await event.edit("Try again after 5 mins.")
+            return
+        In = False
+        tempFedId = ""
+        for x in response.text:
+            if x == "`":
+                if In:
+                    In = False
+                    fedList.append(tempFedId)
+                    tempFedId = ""
+                else:
+                    In = True
+                    
+            elif In:
+                tempFedId += x
 
+    await event.edit(f"UnFbaning in {len(fedList)} feds.")
+    try:
+        await telebot.send_message(chat, f"/start")
+    except:
+        await event.edit("FBAN_GROUP_ID is incorrect.")
+        return
+    await asyncio.sleep(3)
+    for fed in fedList:
+        await telebot.send_message(chat, f"/joinfed {fed}")
+        await asyncio.sleep(3)
+        await telebot.send_message(chat, f"/unfban {FBAN}")
+        await asyncio.sleep(3)
+    await event.edit(f"SuperUnFBan Completed. Affected {len(fedList)} feds.\n#TB")  
+# By @HeisenbergTheDanger, @its_xditya
 
 """
 .superfban <username/userid> <reason>\
