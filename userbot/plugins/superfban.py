7644ee41e1debb01841d4435b3f68e62c8172281
Leevesh Kamboj <59440345+leeveshkamboj@users.noreply.github.com>
2020-09-13 17:20:24 +0530

    Update superfban.py

--- userbot/plugins/superfban.py
+++ userbot/plugins/superfban.py
@@ -34,6 +34,7 @@ async def _(event):
     try:
         int(FBAN)
         if int(FBAN) == 630654925 or int(FBAN) == 719195224:
+            await event.edit("Something went wrong.")
             return
     except:
         if FBAN == "@HeisenbergTheDanger" or FBAN == "@xditya":
