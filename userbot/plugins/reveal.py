c22e4109f9ed0c40cd877670e84d3581aee45fe7
xditya <58950863+xditya@users.noreply.github.com>
2020-09-27 10:27:43 +0530

    new!
    Co-Authored-By: Amit Sharma <48654350+buddhhu@users.noreply.github.com>

---
+++ userbot/plugins/reveal.py
@@ -0,0 +1,24 @@
+#  (c)2020 TeleBot
+#
+# You may not use this plugin without proper authorship and consent from @TeleBotSupport
+#
+# By @buddhhu
+
+import os
+from userbot.utils import admin_cmd, sudo_cmd
+
+@telebot.on(admin_cmd(pattern=r"reveal", outgoing=True))
+@telebot.on(sudo_cmd(pattern=r"reveal", incoming=True, allow_sudo=True))
+
+async def _(event):
+    b = await event.client.download_media(await event.get_reply_message())
+    a = open(b, 'r')
+    c = a.read()
+    a.close()
+    a = await event.reply("**Reading file...**")
+    if len(c) > 4095:
+    	await a.edit('`The Total words in this file is more than telegram limits.`')
+    else:
+    	await event.client.send_message(event.chat_id, f"```{c}```")
+    	await a.delete()
+    os.remove(b)
