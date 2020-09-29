ca3662c8d4c8140ba804d38d91ed654abd0b5b20
xditya <58950863+xditya@users.noreply.github.com>
2020-09-29 11:06:36 +0530

    new ones!
    instagram downloader, whisper, xo-bot, inline stats, mod apps

--- userbot/plugins/botfns.py
+++ userbot/plugins/botfns.py
@@ -7,7 +7,8 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError, UserAlreadyParticipantError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
 from userbot import CMD_HELP
-from userbot.utils import admin_cmd, sudo_cmd
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from var import Var
 
 @telebot.on(admin_cmd(pattern="purl ?(.*)"))
 @telebot.on(sudo_cmd(pattern="purl ?(.*)", allow_sudo=True))
@@ -87,4 +88,74 @@ async def _(event):
           await event.delete()
           await event.client.send_file(event.chat_id, response.message.media)
 
+@telebot.on(admin_cmd(pattern="instadl ?(.*)"))
+@telebot.on(sudo_cmd(pattern="instadl ?(.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.reply("**Reply to a instagram url.**")
+       return
+    reply_message = await event.get_reply_message() 
+    chat = "@instadownloadingbot"
+    sender = reply_message.sender
+    await event.edit("**Downloading the post...**")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1310260390))
+              await event.client.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await a.edit("```Please unblock me (@instadownloadingbot) u Nigga```")
+              return
+          await event.delete()
+          await event.client.send_message(event.chat_id, response.message, reply_to=reply_message)
 
+@borg.on(admin_cmd(pattern="stat$"))
+async def stats(event):
+    if event.fwd_from:
+        return
+    botusername = Var.TG_BOT_USER_NAME_BF_HER
+    noob = "stats"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+
+@borg.on(admin_cmd(pattern="xogame$"))
+async def gamez(event):
+    if event.fwd_from:
+        return
+    botusername = "@xobot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+@borg.on(admin_cmd(pattern="whisepr ?(.*)"))
+async def wspr(event):
+    if event.fwd_from:
+        return
+    wwwspr = event.pattern_match.group(1)
+    botusername = "@whisperBot"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, wwwspr) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+@borg.on(admin_cmd(pattern="mod ?(.*)"))
+async def mod(event):
+    if event.fwd_from:
+        return
+    modr = event.pattern_match.group(1)
+    botusername = "@PremiumAppBot"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, modr) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
