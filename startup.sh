da6910090cf584dfed58769ee78cc20fcde6ea52
xditya <58950863+xditya@users.noreply.github.com>
2020-09-30 19:01:42 +0530

    new lol

--- startup.sh
+++ startup.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-echo "
-  ______   ______     __         ______     ______     ______     ______ 
- /\__  _\ /\  ___\   /\ \       /\  ___\   /\  == \   /\  __ \   /\__  _\
- \/_/\ \/ \ \  __\   \ \ \____  \ \  __\   \ \  __<   \ \ \/\ \  \/_/\ \/
-    \ \_\  \ \_____\  \ \_____\  \ \_____\  \ \_____\  \ \_____\    \ \_\
-     \/_/   \/_____/   \/_____/   \/_____/   \/_____/   \/_____/     \/_/
+echo "   
+
+TTTTTTTTTTTTTTTTTTTTTTTEEEEEEEEEEEEEEEEEEEEEELLLLLLLLLLL             EEEEEEEEEEEEEEEEEEEEEEBBBBBBBBBBBBBBBBB        OOOOOOOOO     TTTTTTTTTTTTTTTTTTTTTTT
+T:::::::::::::::::::::TE::::::::::::::::::::EL:::::::::L             E::::::::::::::::::::EB::::::::::::::::B     OO:::::::::OO   T:::::::::::::::::::::T
+T:::::::::::::::::::::TE::::::::::::::::::::EL:::::::::L             E::::::::::::::::::::EB::::::BBBBBB:::::B  OO:::::::::::::OO T:::::::::::::::::::::T
+T:::::TT:::::::TT:::::TEE::::::EEEEEEEEE::::ELL:::::::LL             EE::::::EEEEEEEEE::::EBB:::::B     B:::::BO:::::::OOO:::::::OT:::::TT:::::::TT:::::T
+TTTTTT  T:::::T  TTTTTT  E:::::E       EEEEEE  L:::::L                 E:::::E       EEEEEE  B::::B     B:::::BO::::::O   O::::::OTTTTTT  T:::::T  TTTTTT
+        T:::::T          E:::::E               L:::::L                 E:::::E               B::::B     B:::::BO:::::O     O:::::O        T:::::T        
+        T:::::T          E::::::EEEEEEEEEE     L:::::L                 E::::::EEEEEEEEEE     B::::BBBBBB:::::B O:::::O     O:::::O        T:::::T        
+        T:::::T          E:::::::::::::::E     L:::::L                 E:::::::::::::::E     B:::::::::::::BB  O:::::O     O:::::O        T:::::T        
+        T:::::T          E:::::::::::::::E     L:::::L                 E:::::::::::::::E     B::::BBBBBB:::::B O:::::O     O:::::O        T:::::T        
+        T:::::T          E::::::EEEEEEEEEE     L:::::L                 E::::::EEEEEEEEEE     B::::B     B:::::BO:::::O     O:::::O        T:::::T        
+        T:::::T          E:::::E               L:::::L                 E:::::E               B::::B     B:::::BO:::::O     O:::::O        T:::::T        
+        T:::::T          E:::::E       EEEEEE  L:::::L         LLLLLL  E:::::E       EEEEEE  B::::B     B:::::BO::::::O   O::::::O        T:::::T        
+      TT:::::::TT      EE::::::EEEEEEEE:::::ELL:::::::LLLLLLLLL:::::LEE::::::EEEEEEEE:::::EBB:::::BBBBBB::::::BO:::::::OOO:::::::O      TT:::::::TT      
+      T:::::::::T      E::::::::::::::::::::EL::::::::::::::::::::::LE::::::::::::::::::::EB:::::::::::::::::B  OO:::::::::::::OO       T:::::::::T      
+      T:::::::::T      E::::::::::::::::::::EL::::::::::::::::::::::LE::::::::::::::::::::EB::::::::::::::::B     OO:::::::::OO         T:::::::::T      
+      TTTTTTTTTTT      EEEEEEEEEEEEEEEEEEEEEELLLLLLLLLLLLLLLLLLLLLLLLEEEEEEEEEEEEEEEEEEEEEEBBBBBBBBBBBBBBBBB        OOOOOOOOO           TTTTTTTTTTT      
+                                                                                                                                                         
 "
 
 python -m userbot
