4393f608385030425c4b892997e6fc887a11cc7c
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2020-10-01 09:51:29 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -24,8 +24,8 @@ async def cmd_list(event):
             if len(string) > 4095:
                 with io.BytesIO(str.encode(string)) as out_file:
                     out_file.name = "cmd.txt"
-                    await bot.send_file(
-                        event.chat_id,
+                    await tgbot.send_file(
+                        event.sender_id,
                         out_file,
                         force_document=True,
                         allow_cache=False,
