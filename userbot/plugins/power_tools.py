3c162d60a3c7de920613aa4b7a01db1b9bb85299
xditya <58950863+xditya@users.noreply.github.com>
2020-10-05 14:15:59 +0530

    Update power_tools.py

--- userbot/plugins/power_tools.py
+++ userbot/plugins/power_tools.py
@@ -17,15 +17,15 @@ async def _(event):
     if event.fwd_from:
         return
     await asyncio.sleep(2)
-    await event.edit("Restarting \n□□□□□□□□□□")
-    await asyncio.sleep(2)
-    await event.edit(f"Restarting \n■■■■□□□□□□  ")
-    await asyncio.sleep(2)
-    await event.edit(f"Restarting \n■■■■■■■■□□   ")
-    await asyncio.sleep(2)
-    await event.edit(f"Done! \n■■■■■■■■■■  ")
-    await asyncio.sleep(2)
-    await event.edit(f"Please give me a minute or two and then use `{CMD_HNDLR}online` or `{CMD_HNDLR}alive`! ")
+    #await event.edit("Restarting \n□□□□□□□□□□")
+    #await asyncio.sleep(2)
+    #await event.edit(f"Restarting \n■■■■□□□□□□  ")
+    #await asyncio.sleep(2)
+    #await event.edit(f"Restarting \n■■■■■■■■□□   ")
+    #await asyncio.sleep(2)
+    #await event.edit(f"Done! \n■■■■■■■■■■  ")
+    #await asyncio.sleep(2)
+    await event.edit(f"__Restarting__ Please give me **a minute or two** and then use `{CMD_HNDLR}alive`! ")
     await borg.disconnect()
     # https://archive.is/im3rt
     os.execl(sys.executable, sys.executable, *sys.argv)
@@ -37,5 +37,5 @@ async def _(event):
 async def _(event):
     if event.fwd_from:
         return
-    await event.edit("TeleBot is turning off ...Manually turn me on later, from heroku.")
+    await event.edit("TeleBot is turning off... Manually turn me on later, from heroku.")
     await borg.disconnect()
