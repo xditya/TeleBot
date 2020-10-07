42b20bf8d59320c029dcbcc964c6e7b7fe2b8397
xditya <58950863+xditya@users.noreply.github.com>
2020-10-07 17:30:30 +0530

    licensing

--- userbot/plugins/mybot/TeleBot.py
+++ userbot/plugins/mybot/TeleBot.py
@@ -1,5 +1,21 @@
+#    TeleBot - UserBot
+#    Copyright (C) 2020 TeleBot
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from telethon import events, Button
-from userbot.plugins.mybot import started, helpmefast, forinfo, forping
+from userbot.plugins import started, helpmefast, forinfo, forping
 
 @tgbot.on(events.NewMessage(pattern="^/start"))
 async def thisfn(event):
