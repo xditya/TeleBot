52b55092a5a6fb4244503f165d5d41611adc5a92
xditya <58950863+xditya@users.noreply.github.com>
2020-10-08 10:52:12 +0530

    Reverting
    Will fix later xD

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -41,7 +41,7 @@ async def _(event):
     if event.fwd_from:
         return
     start = datetime.now()
-    await event.edit_or_reply("🏓 Pong!")
+    await event.edit("🏓 Pong!")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     uptime = get_readable_time((time.time() - StartTime))
