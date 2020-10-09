83170b38c79756d426cba5acd0474757aeba49b7
xditya <58950863+xditya@users.noreply.github.com>
2020-10-10 00:12:23 +0530

    hmm

--- userbot/plugins/mybot/TeleBot.py
+++ userbot/plugins/mybot/TeleBot.py
@@ -15,7 +15,7 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from telethon import events, Button
-from userbot.plugins.mybot import started, helpmefast, forping, fortr
+from userbot.plugins.mybot import started, helpmefast, forping
 import html
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
@@ -183,13 +183,48 @@ async def get_full_user(event):
                 return replied_user, None
             except Exception as e:
                 return None, e
+            
 @tgbot.on(events.NewMessage(pattern="^/tr ?(.*)"))
-async def _(event):    
-    await tgbot.send_message(
-    event.chat_id,
-    message=fortr,
-    buttons = [
-       [Button.url("More", "https://t.me/TeleBotSupport")]
-        ]
-    )
- 
+async def _(event):
+    if event.fwd_from:
+        return
+    if "trim" in event.raw_text:
+        return
+    input_str = event.pattern_match.group(1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        text = previous_message.message
+        lan = input_str or "ml"
+    elif "|" in input_str:
+        lan, text = input_str.split("|")
+    else:
+        await event.edit("`.tr LanguageCode` as reply to a message")
+        return
+    text = emoji.demojize(text.strip())
+    lan = lan.strip()
+    translator = Translator()
+    try:
+        translated = translator.translate(text, dest=lan)
+        after_tr_text = translated.text
+        output_str = """**Translated by TeleBot**\nFrom {} to {}
+{}""".format(
+            translated.src,
+            lan,
+            after_tr_text
+        )    
+        await tgbot.send_message(
+        event.chat_id,
+        message=output_str,
+        buttons = [
+           [Button.url("More", "https://t.me/TeleBotSupport")]
+            ]
+        )
+        except Exception as exc:
+            xx = str(exc)
+            await tgbot.send_message(
+            event.chat_id,
+            message=xx,
+            buttons = [
+           [Button.url("More", "https://t.me/TeleBotSupport")]
+            ]
+            )
