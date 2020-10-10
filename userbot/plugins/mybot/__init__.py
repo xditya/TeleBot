4f36fb09cf86f1dce8b493ea6b3577c0438e3d44
xditya <58950863+xditya@users.noreply.github.com>
2020-10-10 11:38:25 +0530

    Update __init__.py

--- userbot/plugins/mybot/__init__.py
+++ userbot/plugins/mybot/__init__.py
@@ -27,7 +27,7 @@ from datetime import datetime
 started = f"**Welcome To TeleBot**\nHi, this is the assistant bot of {ALIVE_NAME}.\nSend `/help` to see what you can do here!\n\n(c) @TeleBotSupport"
 
 # /help
-helpmefast = "Here are the things that you can do with this bot!\n\n`/info @username` - get information about the user.\n`/ping` - Ping stats\n`/tr <lang_code>` - Use as reply to the text to translate, language codes can be foung [here](https://t.me/TeleBotHelpChat/22678)!.\n`/help` - This menu.\n\n__Set-up your own TeleBot via @TeleBotSupport to get such amazing features and more!__"
+helpmefast = "Here are the things that you can do with this bot!\n\n`/info @username` - get information about the user.\n`/ping` - Ping stats\n`/tr <lang_code>` - Use as reply to the text to translate, language codes can be foung [here](https://t.me/TeleBotHelpChat/22678)!\n`.id` - To get user/sender id.\n`/help` - This menu.\n\n__Set-up your own TeleBot via @TeleBotSupport to get such amazing features and more!__"
 
 # /ping
 def get_readable_time(seconds: int) -> str:
