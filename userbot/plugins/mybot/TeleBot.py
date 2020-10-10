d994ec904540b4a8697eefe3e8cd7d9b5cfd517b
xditya <58950863+xditya@users.noreply.github.com>
2020-10-10 17:11:01 +0530

    heroku stuff

--- userbot/plugins/mybot/TeleBot.py
+++ userbot/plugins/mybot/TeleBot.py
@@ -23,6 +23,15 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 import emoji
 from googletrans import Translator
+import heroku3
+import asyncio
+import os
+import requests
+import math
+from telethon import events, Button
+from userbot import ALIVE_NAME
+
+DEF = str(ALIVE_NAME) if ALIVE_NAME else "TeleBot"
 
 @tgbot.on(events.NewMessage(pattern="^/start"))
 async def thisfn(event):
@@ -264,3 +273,93 @@ async def _(event):
                 [Button.url("More", "https://t.me/TeleBotSupport")]
             ]
         )
+
+Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+
+@tgbot.on(events.NewMessage(pattern=r"^/usage(?: |$)"))
+async def dyno_usage(dyno):
+    """
+        Get your account Dyno Usage
+    """
+    if dyno.from_id == bot.uid:
+        await dyno.edit("`Processing...`")
+        useragent = ('Mozilla/5.0 (Linux; Android 10; SM-G975F) '
+                    'AppleWebKit/537.36 (KHTML, like Gecko) '
+                    'Chrome/80.0.3987.149 Mobile Safari/537.36'
+                    )
+        user_id = Heroku.account().id
+        headers = {
+        'User-Agent': useragent,
+        'Authorization': f'Bearer {Var.HEROKU_API_KEY}',
+        'Accept': 'application/vnd.heroku+json; version=3.account-quotas',
+        }
+        path = "/accounts/" + user_id + "/actions/get-quota"
+        r = requests.get(heroku_api + path, headers=headers)
+        if r.status_code != 200:
+            return await dyno.edit("`Error: something bad happened`\n\n"
+                                f">.`{r.reason}`\n")
+        result = r.json()
+        quota = result['account_quota']
+        quota_used = result['quota_used']
+
+        """ - Used - """
+        remaining_quota = quota - quota_used
+        percentage = math.floor(remaining_quota / quota * 100)
+        minutes_remaining = remaining_quota / 60
+        hours = math.floor(minutes_remaining / 60)
+        minutes = math.floor(minutes_remaining % 60)
+
+        """ - Current - """
+        App = result['apps']
+        try:
+            App[0]['quota_used']
+        except IndexError:
+            AppQuotaUsed = 0
+            AppPercentage = 0
+        else:
+            AppQuotaUsed = App[0]['quota_used'] / 60
+            AppPercentage = math.floor(App[0]['quota_used'] * 100 / quota)
+        AppHours = math.floor(AppQuotaUsed / 60)
+        AppMinutes = math.floor(AppQuotaUsed % 60)
+
+        await asyncio.sleep(1.5)
+    
+        return await tgbot.send_message(dyno.chat_id, "**⚙️ Dyno Usage ⚙️**:\n\n"
+                           f" -> `Dyno usage for`  **{Var.HEROKU_APP_NAME}**:\n"
+                           f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
+                           f"**|**  [`{AppPercentage}`**%**]"
+                           "\n\n"
+                           " -> `Dyno hours quota remaining this month`:\n"
+                           f"     •  `{hours}`**h**  `{minutes}`**m**  "
+                           f"**|**  [`{percentage}`**%**]"
+                           )
+    else:
+        resp = f"This option is available only for my master, {DEF}!"
+        return await tgbot.send_message(dyno.chat_id, message=resp, buttons = [
+           [Button.url("Deploy Your TeleBot", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fxditya%2FTeleBot&template=https%3A%2F%2Fgithub.com%2Fxditya%2FTeleBot")]
+            ])
+
+@tgbot.on(events.NewMessage(pattern=r"^/logs"))
+async def _(givelogs):
+        Heroku = heroku3.from_key(Var.HEROKU_API_KEY)                         
+        app = Heroku.app(Var.HEROKU_APP_NAME)
+        with open('logs.txt', 'w') as log:
+            log.write(app.get_log())
+        if givelogs.from_id == bot.uid:
+            await tgbot.send_file(
+                givelogs.chat_id,
+                "logs.txt",
+                reply_to=givelogs.id,
+                caption="[Heroku] TeleBot Logs",
+                buttons = [
+           [Button.url("Crashed?", "t.me/TeleBotHelpChat")]
+            )
+        else:
+            resp = f"This option is available only for my master, {DEF}!"
+            await tgbot.send_message(givelogs.chat_id, message=resp, buttons = [
+           [Button.url("Deploy Your TeleBot", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fxditya%2FTeleBot&template=https%3A%2F%2Fgithub.com%2Fxditya%2FTeleBot")]
+            ])
+        await asyncio.sleep(5)
+        return os.remove('logs.txt')
+        
