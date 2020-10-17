0e475024193e8987542f85acd7fa1d1b99de7a98
xditya <58950863+xditya@users.noreply.github.com>
2020-10-17 23:40:27 +0530

    hmm...

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -5,11 +5,15 @@ from var import Var
 from userbot.utils import load_module, start_mybot
 from pathlib import Path
 import telethon.utils
+from userbot import CMD_HNDLR
 
 async def add_bot(bot_token):
     await bot.start(bot_token)
     bot.me = await bot.get_me() 
     bot.uid = telethon.utils.get_peer_id(bot.me)
+
+TELE = Var.PRIVATE_GROUP_ID
+MYBOT = Var.TG_BOT_USER_NAME_BF_HER
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
@@ -29,7 +33,7 @@ else:
         print("Startup Completed")
     else:
         bot.start()
-    
+await telebot.send_message(TELE, "Deploying TeleBot...")    
 import glob
 path = 'userbot/plugins/*.py'
 files = glob.glob(path)
@@ -39,8 +43,9 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 print("TeleBot has been deployed! ")
-
+await telebot.send_message(TELE, "TeleBot has been deployed...")
 print("Setting up TGBot")
+await telebot.send_message(TELE, f"Setting up @{MYBOT}")
 path = "userbot/plugins/mybot/*.py"
 files = glob.glob(path)
 for name in files:
@@ -49,9 +54,10 @@ for name in files:
         shortname = path1.stem
         start_mybot(shortname.replace(".py", ""))
 print("TGBot has been set up!")      
-
+await telebot.send_message(TELE, f"@{MYBOT} has been set up. Send ```/start``` to the bot 😁")")
 print("TeleBot has been fully deployed! Do Visit @TeleBotSupport")
-
+await telebot.send_message(TELE, f"TeleBot has been fully deployed. {MYBOT} has been set-up completely.")
+await telebot.send_message(TELE, f"Send ```{CMD_HNDLR}alive``` to see if the bot is working.\n\nDo add @{MYBOT} to this group and make it admin, else some features won't work")
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
