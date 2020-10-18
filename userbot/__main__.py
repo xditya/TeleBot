0e188761e86e44e564dd9b762c807057ae18b1f2
xditya <58950863+xditya@users.noreply.github.com>
2020-10-18 16:59:41 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -16,19 +16,19 @@ async def add_bot(bot_token):
     bot.uid = telethon.utils.get_peer_id(bot.me)
 
 async def startup_log_telebot_start(bot_name):
-    await telebot.send_message(TELE, "Deploying TeleBot...")
+    await bot.send_message(TELE, "Deploying TeleBot...")
 
 async def startup_log_tgbot_start(bot_name):
-    await telebot.send_message(TELE, f"Setting up @{BOTNAME}...")
+    await bot.send_message(TELE, f"Setting up @{BOTNAME}...")
     
 async def startup_log_telebot_done(bot_name):
-    await telebot.send_message(TELE, "TeleBot has been deployed...")
+    await bot.send_message(TELE, "TeleBot has been deployed...")
         
 async def startup_log_tgbot_done(bot_name):
-    await telebot.send_message(TELE, f"@{BOTNAME} has been set up! Send `/start' to it 😁.")
+    await bot.send_message(TELE, f"@{BOTNAME} has been set up! Send `/start' to it 😁.")
 
 async def startup_log_all_done(bot_name):
-    await telebot.send_message(TELE, f"TeleBot has been deployed, @{BOTNAME} has been set up.\nSend `{CMD_HNDLR}alive` to see if the bot is working.\nDo add @{BOTNAME} to this group and make it adim for enabling all the features of **TeleBot**")
+    await bot.send_message(TELE, f"TeleBot has been deployed, @{BOTNAME} has been set up.\nSend `{CMD_HNDLR}alive` to see if the bot is working.\nDo add @{BOTNAME} to this group and make it adim for enabling all the features of **TeleBot**")
     
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
