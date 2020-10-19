0bb0502e6d583bb2f572610c69a6c4b0e5e206b7
xditya <58950863+xditya@users.noreply.github.com>
2020-10-19 06:39:42 +0530

    hmm

--- userbot/plugins/pmsecurity.py
+++ userbot/plugins/pmsecurity.py
@@ -159,7 +159,7 @@ async def on_new_private_message(event):
         # userbot's should not reply to other userbot's
         # https://core.telegram.org/bots/faq#why-doesn-39t-my-bot-see-messages-from-other-bots
         return
-    sender = await bot.get_entity(chat_id)
+    sender = await event.get_chat() 
 
     if chat_id == bot.uid:
 
