3b3624a2af15d85a0fdc8a32da4a813daeb6b141
xditya <58950863+xditya@users.noreply.github.com>
2020-10-20 11:04:29 +0000

    pylint: auto fixes

--- userbot/plugins/autopic.py
+++ userbot/plugins/autopic.py
@@ -11,43 +11,54 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Affero General Public License for more details.
 
+import asyncio
+
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #    By @buddhhu
 #
-import os, asyncio, shutil
-from userbot import *
+import os
+import shutil
 from datetime import datetime
+
 from PIL import Image, ImageDraw, ImageFont
-from pySmartDL import SmartDL
 from telethon.tl import functions
-from userbot import AUTOPIC_TEXT, AUTO_PIC_FONT, AUTOPIC_FONT_COLOUR
+
+from userbot import *
+from userbot import AUTO_PIC_FONT, AUTOPIC_FONT_COLOUR, AUTOPIC_TEXT
 
 fntz = str(AUTO_PIC_FONT) if AUTO_PIC_FONT else "DejaVuSans.ttf"
 FONT_FILE_TO_USE = f"fonts/{fntz}"
-AUTOPIC_TEXT = str(AUTOPIC_TEXT) if AUTOPIC_TEXT else "Life Is too Short.\n And so is your TG account."
+AUTOPIC_TEXT = (
+    str(AUTOPIC_TEXT)
+    if AUTOPIC_TEXT
+    else "Life Is too Short.\n And so is your TG account."
+)
 COLOUR = str(AUTOPIC_FONT_COLOUR) if AUTOPIC_FONT_COLOUR else (255, 255, 255)
+
 
 @telebot.on(admin_cmd(pattern="autopic"))
 async def autopic(event):
-	await event.edit("**Autopic has been enabled!!!**")
-	a = await event.get_reply_message()
-	downloaded_file_name = "userbot/original_pic.png"
-	downloader = await telebot.download_media(a, downloaded_file_name)
-	photo = "userbot/photo_pfp.png"
-	while True:
-		shutil.copy(downloaded_file_name, photo)
-		current_time = datetime.now().strftime(f"Time: %H:%M \nDate: %d.%m.%y \n{AUTOPIC_TEXT}")
-		img = Image.open(photo)
-		drawn_text = ImageDraw.Draw(img)
-		fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
-		color = (COLOUR)
-		drawn_text.text((95, 250), current_time, font=fnt, fill=color)
-		img.save(photo)
-		file = await event.client.upload_file(photo)
-		try:
-			await event.client(functions.photos.UploadProfilePhotoRequest(file))
-			os.remove(photo)
-			await asyncio.sleep(60)
-		except:
-			return
+    await event.edit("**Autopic has been enabled!!!**")
+    a = await event.get_reply_message()
+    downloaded_file_name = "userbot/original_pic.png"
+    await telebot.download_media(a, downloaded_file_name)
+    photo = "userbot/photo_pfp.png"
+    while True:
+        shutil.copy(downloaded_file_name, photo)
+        current_time = datetime.now().strftime(
+            f"Time: %H:%M \nDate: %d.%m.%y \n{AUTOPIC_TEXT}"
+        )
+        img = Image.open(photo)
+        drawn_text = ImageDraw.Draw(img)
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+        color = COLOUR
+        drawn_text.text((95, 250), current_time, font=fnt, fill=color)
+        img.save(photo)
+        file = await event.client.upload_file(photo)
+        try:
+            await event.client(functions.photos.UploadProfilePhotoRequest(file))
+            os.remove(photo)
+            await asyncio.sleep(60)
+        except BaseException:
+            return
