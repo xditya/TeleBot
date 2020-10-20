27fa8dbd80822de35fb96ae7902b4b2dfd4044f8
xditya <58950863+xditya@users.noreply.github.com>
2020-10-20 18:33:14 +0530

    Update admin.py

--- userbot/plugins/admin.py
+++ userbot/plugins/admin.py
@@ -1,7 +1,3 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
 """
 Userbot module to help you manage a group
 """
@@ -255,7 +251,7 @@ async def _(event):
     except (Exception) as exc:
         xx = await event.edit(xx, str(exc))
     else:
-        xx = await event.edit(xx f"{input_cmd}ned Successfully!")
+        xx = await event.edit(xx, f"{input_cmd}ned Successfully!")
 
 
 @telebot.on(admin_cmd(pattern="pgs ?(.*)"))
