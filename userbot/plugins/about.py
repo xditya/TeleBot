b93516c08483e2709e20db88653454c5966ca1e8
xditya <58950863+xditya@users.noreply.github.com>
2020-10-21 07:21:37 +0530

    Update about.py

--- userbot/plugins/about.py
+++ userbot/plugins/about.py
@@ -12,7 +12,7 @@ from userbot.utils import admin_cmd
 
 
 @telebot.on(admin_cmd(pattern="info"))
-@telebot.on(sudo_cmd(pattern="info"))
+@telebot.on(sudo_cmd(pattern="info", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
