05dc6849081f7a659e1ec73b643237bb7201b3d9
xditya <58950863+xditya@users.noreply.github.com>
2020-10-21 06:30:30 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -2,9 +2,8 @@ import sys
 import math
 from userbot import bot
 from telethon import events
-from var import Var
 from pathlib import Path
-from userbot.uniborgConfig import Config
+from userbot.telebotConfig import Var, Config
 from userbot import LOAD_PLUG
 from userbot import CMD_LIST
 import re
