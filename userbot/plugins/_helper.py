6eb4051b606b46b7b0c23126ce37506807ca4970
xditya <58950863+xditya@users.noreply.github.com>
2020-10-22 14:27:46 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -46,7 +46,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[TeleBot](https://xditya.gitbook.io/telebot/)**\n\n"""
+            help_string = f"TeleBot Help Menu"
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
             )
