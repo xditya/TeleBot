242a71d42e15a192da89e001998467f004bf4c51
xditya <58950863+xditya@users.noreply.github.com>
2020-10-23 07:19:21 +0530

    Create sudo.py

---
+++ userbot/plugins/sudo.py
@@ -0,0 +1,38 @@
+#    TeleBot - UserBot
+#    Copyright (C) 2020 TeleBot
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+from ..telebotConfig import Config, Var
+
+@telebot.on(admin_cmd(pattern="sudo"))
+async def sudo(event):
+    sudo = "True" if Config.SUDO_USERS else "False"
+    users = os.environ.get("SUDO_USERS", None)
+    if sudo == "True":
+        await eor(event, f"**TeleBot**\nSudo - `Enabled`\nSudo user(s) - `{users}`")
+    else:
+        await eor(event, f"**TeleBot**\nSudo - `Disabled`")
+
+@telebot.on(admin_cmd(pattern="prefix"))
+async def handler(event):
+    hndlr = Config.CMD_HNDLR
+    if hndlr == "\.":
+        x = "."
+    else:
+        x = Config.CMD_HNDLR
+    
+    sudohndlr = Config.SUDO_HNDLR
+    await eor(event, f"Command Handler - {x}\nSudo Handler - {sudohndlr}")
