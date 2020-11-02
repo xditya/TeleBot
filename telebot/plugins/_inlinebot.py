44b81cb6f9add90b2a162b7dc2b628a779d10f88
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-02 11:10:03 +0530

    Update _inlinebot.py

--- telebot/plugins/_inlinebot.py
+++ telebot/plugins/_inlinebot.py
@@ -287,7 +287,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                         help_string += i
                     help_string += "\n"
                 else:
-                    help_string "Commands Available -\n\n"
+                    help_string += "Commands Available -\n\n"
                     for i in CMD_LIST[plugin_name]:
                         help_string += i
                         help_string += "\n"
