6568a22871f087844fb39635cb3f3e5adf7f603a
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-04 09:31:50 +0530

    Update __main__.py

--- telebot/__main__.py
+++ telebot/__main__.py
@@ -19,7 +19,10 @@ async def add_bot(bot_token):
 
 
 async def startup_log_all_done():
-    await bot.send_message(TELE, f"**TeleBot has been deployed,** @{BOTNAME} **has been set up.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\n__Do add** @{BOTNAME} **to this group and make it admin for enabling all the features of TeleBot**__")
+    try:
+        await bot.send_message(TELE, f"**TeleBot has been deployed,** @{BOTNAME} **has been set up.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\n__Do add** @{BOTNAME} **to this group and make it admin for enabling all the features of TeleBot**__")
+    except:
+        print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
