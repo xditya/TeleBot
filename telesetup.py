a440123b22f74c7c04a7e5ca1d22c29831a769bd
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-04 14:04:29 +0530

    Update telesetup.py

--- telesetup.py
+++ telesetup.py
@@ -12,8 +12,17 @@ Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details
 For TeleBot""")
+print("")
+
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    tele = client.send_message("me", session_str)
+    tele.reply("The above is the `STRING_SESSION` for your current session.\n@TeleBotSupport")
+    print("")
+    print("")
+    print("Below is the STRING_SESSION. You can also find it in your Telegram Saved Messages.")
+    print("")
+    print("")
     print(client.session.save())
