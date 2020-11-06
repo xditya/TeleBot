6d04eae8523d2082e12953789aa6b1e445b12532
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-06 10:16:13 +0530

    Update __init__.py

--- telebot/__init__.py
+++ telebot/__init__.py
@@ -17,6 +17,7 @@ else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 
+telebot = bot
 StartTime = time.time()
 telever = "4.7"
 ALIVE_NAME = telebot.me.first_name
