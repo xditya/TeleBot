c530ed24e6a1fbfba5c97f94402eada4cfc6954f
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-06 08:58:23 +0530

    Update _inline.py

--- telebot/plugins/_inline.py
+++ telebot/plugins/_inline.py
@@ -317,20 +317,16 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             reply_pop_up_alert = "Please get your own Userbot, and don't use mine!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-
 def paginate_help(page_number, loaded_plugins, prefix):
-    number_of_rows = HELP_ROWS
-    number_of_cols = HELP_COLOUMNS
-    tele = CUSTOM_HELP_EMOJI
+    number_of_rows = 5
+    number_of_cols = 2
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [
-        custom.Button.inline(
-            "{} {} {}".format(tele, x, tele), data="us_plugin_{}".format(x)
-        )
+        custom.Button.inline("{} {}".format("⚡", x, "⚡"), data="us_plugin_{}".format(x))
         for x in helpable_plugins
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
@@ -339,19 +335,19 @@ def paginate_help(page_number, loaded_plugins, prefix):
     max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
-        pairs = (
-            pairs[modulo_page * number_of_rows : number_of_rows * (modulo_page + 1)]
-            + [
-                (
-                    custom.Button.inline(
-                        "⏮️ Previous", data="{}_prev({})".format(prefix, modulo_page)
-                    ),
-                    custom.Button.inline("Close", data="close"),
-                    custom.Button.inline(
-                        "Next ⏭️", data="{}_next({})".format(prefix, modulo_page)
-                    ),
-                )
-            ]
+        pairs = pairs[
+            modulo_page * number_of_rows : number_of_rows * (modulo_page + 1)
+        ] + [
+            (
+                custom.Button.inline(
+                    "⏮️ Previous", data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                custom.Button.inline("Close", data="close"),
+                custom.Button.inline(
+                    "Next ⏭️", data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
+        ]
     return pairs
 
 
