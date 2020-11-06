56d23621fa78535dd7cda4a9d39b9378427ccd0e
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-06 06:08:12 +0530

    Update superfban.py

--- telebot/plugins/superfban.py
+++ telebot/plugins/superfban.py
@@ -1,7 +1,18 @@
-#  (c)2020 TeleBot
-#
-# You may not use this plugin without proper authorship and consent from @TeleBotSupport
-#
+#    TeleBot - UserBot
+#    Copyright (C) 2020 TeleBot
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import asyncio
 
@@ -236,4 +247,3 @@ CMD_HELP.update(
         \n\nGet help - @TeleBotSupport."
     }
 )
-"""
