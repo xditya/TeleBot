d9c44c9b1fe5ac731982f1a5983401269257d334
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-09 11:34:34 +0530

    Update _help.py

--- telebot/plugins/_help.py
+++ telebot/plugins/_help.py
@@ -72,10 +72,10 @@ async def cmd_list(event):
                 await event.edit(input_str + " is not a valid plugin!")
         else:
             help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[TeleBot](https://xditya.gitbook.io/telebot/)**\n\n"""
-            results = await bot.inline_query(  # pylint:disable=E0602
-                tgbotusername, help_string
-            )
             try:
+                results = await bot.inline_query(  # pylint:disable=E0602
+                    tgbotusername, help_string
+                )
                 await results[0].click(
                     event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
                 )
