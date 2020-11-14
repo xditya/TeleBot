076d4c1104615f18a100681ba29175fca72328af
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-14 15:46:57 +0530

    Update __init__.py

--- telebot/plugins/mybot/sql/__init__.py
+++ telebot/plugins/mybot/sql/__init__.py
@@ -4,27 +4,26 @@
 
 """ init SQL """
 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-from telebot.telebotConfig import DB_URI
+
+# the secret configuration specific things
+from telebot.telebotConfig import Var
 
 
 def start() -> scoped_session:
-    """ returns SQLAlchemy ScopedSession """
-    engine = create_engine(DB_URI)
+    engine = create_engine(Var.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
-    return scoped_session(
-        sessionmaker(
-            bind=engine,
-            autoflush=False
-        )
-    )
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
 try:
     BASE = declarative_base()
     SESSION = start()
-except BaseException:
-    pass
+except AttributeError as e:
+    # this is a dirty way for the work-around required for #23
+    print("DB_URI is not configured. Features depending on the database might have issues.")
+    print(str(e))
