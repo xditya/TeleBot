4764cead70b79ff7272111d7551e305ab2e3a3da
xditya <58950863+xditya@users.noreply.github.com>
2020-11-14 18:28:50 +0000

    pylint: auto fixes

--- telebot/__init__.py
+++ telebot/__init__.py
@@ -88,8 +88,12 @@ if bool(ENV):
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
 
     # Chrome Driver and Headless Google Chrome Binaries
-    CHROME_DRIVER = os.environ.get("CHROME_DRIVER", "/app/.chromedriver/bin/chromedriver")
-    GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome")
+    CHROME_DRIVER = os.environ.get(
+        "CHROME_DRIVER",
+        "/app/.chromedriver/bin/chromedriver")
+    GOOGLE_CHROME_BIN = os.environ.get(
+        "GOOGLE_CHROME_BIN",
+        "/app/.apt/usr/bin/google-chrome")
 
     # For bit.ly plugin
     BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
