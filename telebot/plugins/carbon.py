06530c1186228fed2b3cbdea494a4b0bfef3e7e4
VISHNU CS <mailvishnucs@gmail.com>
2020-11-15 00:02:40 +0530

    Update carbon.py

--- telebot/plugins/carbon.py
+++ telebot/plugins/carbon.py
@@ -46,14 +46,14 @@ async def carbon_api(e):
         url = CARBON.format(code=code, lang=CARBONLANG)
         chrome_options = Options()
         chrome_options.add_argument("--headless")
-        chrome_options.binary_location = GOOGLE_CHROME_BIN
+        chrome_options.binary_location = ("/app/.apt/usr/bin/google-chrome")
         chrome_options.add_argument("--window-size=1920x1080")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-gpu")
         prefs = {"download.default_directory": "./"}
         chrome_options.add_experimental_option("prefs", prefs)
-        driver = webdriver.Chrome(executable_path=CHROME_DRIVER, options=chrome_options)
+        driver = webdriver.Chrome(executable_path=("/app/.chromedriver/bin/chromedriver"), options=chrome_options)
         driver.get(url)
         await eor(e, "`Be Patient...\n50%`")
         download_path = "./"
