fa7931e82ca37d5b3f1879b3a93d9306f54c5cf1
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-14 14:46:00 +0530

    Update whois.py

--- telebot/plugins/whois.py
+++ telebot/plugins/whois.py
@@ -25,7 +25,7 @@ import os
 from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
-from userbot import CMD_HELP
+from telebot import CMD_HELP
 
 TMP_DOWNLOAD_DIRECTORY = "./"
 
