af0bc0420960911c79853939a8e690954abf010a
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-15 14:16:18 +0530

    Create start.py

---
+++ telebot/plugins/mybot/pmbot/start.py
@@ -0,0 +1,17 @@
+from telebot.plugins.mybot import *
+from telethon import events, Button
+
+# start-other-enabled
+
+@tgbot.on(events.NewMessage(pattern="^/start"))
+async def start_all(event):
+    if event.sender_id == OWNER_ID:
+        return
+    else:
+        await tgbot.send_message(event.chat_id,
+                                 startotherena,
+                                 buttons=[
+                                     (Button.url(
+                                         "TeleBot",
+                                         url="https://github.com/xditya/TeleBot"))]
+                                 )
