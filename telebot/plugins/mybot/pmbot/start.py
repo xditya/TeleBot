fb3045b97d0ec2474a04b8f95fbbb6dac088049f
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-16 06:12:18 +0530

    Update start.py

--- telebot/plugins/mybot/pmbot/start.py
+++ telebot/plugins/mybot/pmbot/start.py
@@ -1,12 +1,13 @@
 from telebot.plugins.mybot import *
 from telethon import events, Button
+from telebot.telebotConfig import Var
 
 # start-other-enabled
 
 
 @tgbot.on(events.NewMessage(pattern="^/start"))
 async def start_all(event):
-    if event.sender_id == OWNER_ID:
+    if event.sender_id == OWNER_ID and Var.LOAD_MYBOT == "False":
         return
     else:
         await tgbot.send_message(event.chat_id,
