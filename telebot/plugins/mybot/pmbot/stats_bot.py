f2816055db3223ecf20fd2a585d887785933fb17
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-16 07:26:47 +0530

    Update stats_bot.py

--- telebot/plugins/mybot/pmbot/stats_bot.py
+++ telebot/plugins/mybot/pmbot/stats_bot.py
@@ -22,8 +22,8 @@ from telebot.plugins import OWNER_ID
 
 @tgbot.on(events.NewMessage(pattern="^/stats", from_users=OWNER_ID))
 async def tele(event):
-    allu = all_users()
-    blu = all_bl_users()
+    allu = len(all_users())
+    blu = len(all_bl_users())
     await tgbot.send_message(event.chat_id,
                              "Here is the stats for your bot:\nTotal Users = {}\nBlacklisted Users = {}".format(allu, blu)
                              )
