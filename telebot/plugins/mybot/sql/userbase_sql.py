bd15eb6c86620e6e7eebbebc1bd1ed29e180e5ab
xditya <58950863+xditya@users.noreply.github.com>
2020-11-21 06:39:40 +0000

    pylint: auto fixes

--- telebot/plugins/mybot/sql/userbase_sql.py
+++ telebot/plugins/mybot/sql/userbase_sql.py
@@ -2,6 +2,7 @@
 from sqlalchemy import Column, String
 from . import BASE, SESSION
 
+
 class userbase(BASE):
     __tablename__ = "UserBase"
     chat_id = Column(String(14), primary_key=True)
@@ -9,22 +10,27 @@ class userbase(BASE):
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
+
 userbase.__table__.create(checkfirst=True)
+
 
 def add_to_userbase(chat_id: int):
     __user = userbase(str(chat_id))
     SESSION.add(__user)
     SESSION.commit()
 
+
 def full_userbase():
     users = SESSION.query(userbase).all()
     SESSION.close()
     return users
 
+
 def present_in_userbase(chat_id):
     try:
-        return SESSION.query(userbase).filter(userbase.chat_id == str(chat_id)).one()
-    except:
+        return SESSION.query(userbase).filter(
+            userbase.chat_id == str(chat_id)).one()
+    except BaseException:
         return None
     finally:
         SESSION.close()
