f9326beea38c60b5c318f2467fee9ead3ba996d1
xditya <58950863+xditya@users.noreply.github.com>
2020-11-22 10:58:45 +0000

    pylint: auto fixes

--- telebot/misc/tools.py
+++ telebot/misc/tools.py
@@ -5,20 +5,27 @@ import shlex
 from os.path import basename, join
 from typing import Optional, Tuple
 
-CACHE_DL = ./cache/
+CACHE_DL = . / cache/
 
-# original --> https://github.com/UsergeTeam/Userge/blob/beta/userge/utils/tools.py
+# original -->
+# https://github.com/UsergeTeam/Userge/blob/beta/userge/utils/tools.py
+
 
 async def take_screen_shot(video_file: str, duration: int, path: str = '') -> Optional[str]:
     """ take a screenshot """
-    print('[[[Extracting a frame from %s ||| Video duration => %s]]]', video_file, duration)
+    print(
+        '[[[Extracting a frame from %s ||| Video duration => %s]]]',
+        video_file,
+        duration)
     ttl = duration // 2
-    thumb_image_path = path or os.path.join(CACHE_DL, f"{basename(video_file)}.jpg")
+    thumb_image_path = path or os.path.join(
+        CACHE_DL, f"{basename(video_file)}.jpg")
     command = f"ffmpeg -ss {ttl} -i '{video_file}' -vframes 1 '{thumb_image_path}'"
     err = (await runcmd(command))[1]
     if err:
         print(err)
     return thumb_image_path if os.path.exists(thumb_image_path) else None
+
 
 async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
     """ run command in terminal """
