8ea7d45b4feb29520bb2b6d190222b91c7034b09
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-22 19:58:12 +0530

    Update outgoing.py

--- telebot/plugins/mybot/pmbot/outgoing.py
+++ telebot/plugins/mybot/pmbot/outgoing.py
@@ -1,20 +1,5 @@
-#    TeleBot - UserBot
-#    Copyright (C) 2020 TeleBot
+# by @StarkGang 😋 
 
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-# OutGoing Message.
 from telebot.plugins.mybot.sql.users_sql import get_user_id
 from telethon import events
 from telethon.utils import pack_bot_file_id
