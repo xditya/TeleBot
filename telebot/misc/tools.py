f29a68cca9e771a4d92e85fd31b17e2278bf81e8
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-22 16:26:53 +0530

    Create tools.py

---
+++ telebot/misc/tools.py
@@ -0,0 +1,33 @@
+# helper functions.
+import asyncio
+import os
+import shlex
+from os.path import basename, join
+from typing import Optional, Tuple
+
+CACHE_DL = ./cache/
+
+# original --> https://github.com/UsergeTeam/Userge/blob/beta/userge/utils/tools.py
+
+async def take_screen_shot(video_file: str, duration: int, path: str = '') -> Optional[str]:
+    """ take a screenshot """
+    print('[[[Extracting a frame from %s ||| Video duration => %s]]]', video_file, duration)
+    ttl = duration // 2
+    thumb_image_path = path or os.path.join(CACHE_DL, f"{basename(video_file)}.jpg")
+    command = f"ffmpeg -ss {ttl} -i '{video_file}' -vframes 1 '{thumb_image_path}'"
+    err = (await runcmd(command))[1]
+    if err:
+        print(err)
+    return thumb_image_path if os.path.exists(thumb_image_path) else None
+
+async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+    """ run command in terminal """
+    args = shlex.split(cmd)
+    process = await asyncio.create_subprocess_exec(*args,
+                                                   stdout=asyncio.subprocess.PIPE,
+                                                   stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await process.communicate()
+    return (stdout.decode('utf-8', 'replace').strip(),
+            stderr.decode('utf-8', 'replace').strip(),
+            process.returncode,
+            process.pid)
