643f16c92bb8f77ef554f9805dce6f88962c5466
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-26 21:00:33 +0530

    Update _inline.py

--- telebot/plugins/_inline.py
+++ telebot/plugins/_inline.py
@@ -192,7 +192,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             await event.edit("Menu Re-opened", buttons=buttons)
         else:
             reply_pop_up_alert = "This bot ain't for u!!"
-            wait event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"req")))
     async def on_pm_click(event):
