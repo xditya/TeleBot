8e37481dca7c0f3cfe1460c2f08792cf6ac88f90
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-26 20:48:59 +0530

    Update _inline.py

--- telebot/plugins/_inline.py
+++ telebot/plugins/_inline.py
@@ -188,12 +188,11 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"reopen")))
     async def megic(event):
         if event.query.user_id == bot.uid :
-                current_page_number=0
-                buttons = paginate_help(0, CMD_LIST, "helpme")
-                await event.edit("Menu Re-opened", buttons=buttons)
-            else:
-                reply_pop_up_alert = "This bot ain't for u!!"
-                await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+            buttons = paginate_help(0, CMD_LIST, "helpme")
+            await event.edit("Menu Re-opened", buttons=buttons)
+        else:
+            reply_pop_up_alert = "This bot ain't for u!!"
+            wait event.answer(reply_pop_up_alert, cache_time=0, alert=True)
     
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"req")))
     async def on_pm_click(event):
