9d5d52a36918b51bea5e07d6dc169e6d17074d9d
xditya <58950863+xditya@users.noreply.github.com>
2020-11-26 13:44:00 +0000

    pylint: auto fixes

--- telebot/plugins/song.py
+++ telebot/plugins/song.py
@@ -4,26 +4,31 @@ Music scrapper - Scrapps of SMD Database
 # Better now - @its_xditya
 # Based off plugin by @okay_retard && @hellboi_atul
 
-from telebot.plugins import TELE_NAME, OWNER_ID
+from telethon.errors.rpcerrorlist import UserAlreadyParticipantError
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from telethon.tl.types import InputMessagesFilterMusic
-from telethon.tl.functions.messages import ImportChatInviteRequest
-from telethon.errors.rpcerrorlist import UserAlreadyParticipantError
+
+from telebot.plugins import OWNER_ID, TELE_NAME
 
 PROF = f"[{TELE_NAME}](tg://user?id={OWNER_ID})"
+
 
 @borg.on(admin_cmd("song ?(.*)"))
 async def _(event):
     try:
-       await telebot(ImportChatInviteRequest('DdR2SUvJPBouSW4QlbJU4g'))
+        await telebot(ImportChatInviteRequest("DdR2SUvJPBouSW4QlbJU4g"))
     except UserAlreadyParticipantError:
         pass
     except Exception as e:
-        await event.edit(f"`Hmm.. Some unknown error occured!\nAborting...\nError - {str(e)}`")
+        await event.edit(
+            f"`Hmm.. Some unknown error occured!\nAborting...\nError - {str(e)}`"
+        )
         return
     name = event.pattern_match.group(1)
     if not name:
-        await event.edit("Song donwloader.\nSyntax - `.song name`\nFor better results, use Artist Name -Song Name.")
+        await event.edit(
+            "Song donwloader.\nSyntax - `.song name`\nFor better results, use Artist Name -Song Name."
+        )
         return
     chat = -1001271479322
     current_chat = event.chat_id
@@ -33,11 +38,14 @@ async def _(event):
 ⫸ **Uploaded by** {}
 """
     try:
-       async for event in telebot.iter_messages(chat, search=name, limit=1, filter=InputMessagesFilterMusic):
-                    ok = cap.format(event.message, PROF)
-                    await telebot.delete_messages(current_chat, current_msg)
-                    await telebot.send_file(current_chat, event, caption=ok)
-    except:
-             await event.reply(f"`Song, {name}, not found. For better results, use Artist Name -Song Name.`")
-             return
-    
+        async for event in telebot.iter_messages(
+            chat, search=name, limit=1, filter=InputMessagesFilterMusic
+        ):
+            ok = cap.format(event.message, PROF)
+            await telebot.delete_messages(current_chat, current_msg)
+            await telebot.send_file(current_chat, event, caption=ok)
+    except BaseException:
+        await event.reply(
+            f"`Song, {name}, not found. For better results, use Artist Name -Song Name.`"
+        )
+        return
