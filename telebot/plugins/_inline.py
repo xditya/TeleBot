6b872df25df005e12905bd6ee64e877695d07f44
xditya <58950863+xditya@users.noreply.github.com>
2020-11-26 15:31:43 +0000

    pylint: auto fixes

--- telebot/plugins/_inline.py
+++ telebot/plugins/_inline.py
@@ -187,13 +187,13 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"reopen")))
     async def megic(event):
-        if event.query.user_id == bot.uid :
+        if event.query.user_id == bot.uid:
             buttons = paginate_help(0, CMD_LIST, "helpme")
             await event.edit("Menu Re-opened", buttons=buttons)
         else:
             reply_pop_up_alert = "This bot ain't for u!!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-    
+
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"req")))
     async def on_pm_click(event):
         if event.query.user_id == bot.uid:
