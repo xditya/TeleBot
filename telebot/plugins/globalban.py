22061da196ba108c7feabe9a58e5f722ac85e16d
xditya <58950863+xditya@users.noreply.github.com>
2020-12-06 10:37:12 +0000

    pylint: auto fixes

--- telebot/plugins/globalban.py
+++ telebot/plugins/globalban.py
@@ -192,8 +192,8 @@ async def list(event):
         await doing.edit(userlist)
 
 
-async def get_user_from_event(event):  
-    args = event.pattern_match.group(1).split(':', 1)
+async def get_user_from_event(event):
+    args = event.pattern_match.group(1).split(":", 1)
     extra = None
     if event.reply_to_msg_id and not len(args) == 2:
         previous_message = await event.get_reply_message()
@@ -210,17 +210,15 @@ async def get_user_from_event(event):
             return
         if event.message.entities is not None:
             probable_user_mention_entity = event.message.entities[0]
-            if isinstance(probable_user_mention_entity,
-                          MessageEntityMentionName):
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
                 user_id = probable_user_mention_entity.user_id
                 user_obj = await event.client.get_entity(user_id)
                 return user_obj
         try:
             user_obj = await event.client.get_entity(user)
         except Exception as err:
-            return await event.edit("Failed \n **Error**\n", str(err))           
+            return await event.edit("Failed \n **Error**\n", str(err))
     return user_obj, extra
-
 
 
 # supposed to be a gban watch
