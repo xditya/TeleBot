95f94b1b8a3a296cea072f35e39c78f0d7f6e7ec
Aditya <58950863+xditya@users.noreply.github.com>
2020-12-07 09:00:02 +0530

    Hmm

--- telebot/utils.py
+++ telebot/utils.py
@@ -10,8 +10,8 @@ import re
 import logging
 import inspect
 
-handler = Config.CMD_HNDLR if Config.CMD_HNDLR else r"\."
-sudo_hndlr = Config.SUDO_HNDLR if Config.SUDO_HNDLR else "!"
+handler = Var.CMD_HNDLR if Var.CMD_HNDLR else r"\."
+sudo_hndlr = Var.SUDO_HNDLR if Var.SUDO_HNDLR else "!"
 
 
 def command(**args):
