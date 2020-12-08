5429b4ef08fb25ad7d4b04b569e4550078565ec7
xditya <58950863+xditya@users.noreply.github.com>
2020-12-08 05:03:32 +0000

    pylint: auto fixes

--- telebot/plugins/mybot/start.py
+++ telebot/plugins/mybot/start.py
@@ -409,7 +409,8 @@ async def custommm(event):
                     ]
 
 # pylint: disable=oof
-@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"alive_cus")))  # pylint: disable=oof
+@ tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"alive_cus"))
+           )  # pylint: disable=oof
 async def alv_cs(event):
     await event.edit("Here are the avaialble customisations for alive",
                     buttons=[
@@ -418,7 +419,8 @@ async def alv_cs(event):
                     ])
 
  # pylint: disable=oof
-@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"alv_txt")))  # pylint: disable=oof
+@ tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"alv_txt"))
+           )  # pylint: disable=oof
 async def alv_txt(event):
     if event.sender_id == OWNER_ID:
         await event.delete()
@@ -447,7 +449,8 @@ async def alv_txt(event):
         await event.answer("You can't use this bot.", alert=True)
 
  # pylint: disable=oof
-@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"alv_pic")))  # pylint: disable=oof
+@ tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"alv_pic"))
+           )  # pylint: disable=oof
 async def alv_pic(event):
     if event.sender_id == OWNER_ID:
         await event.delete()
